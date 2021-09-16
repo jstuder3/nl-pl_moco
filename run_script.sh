0122d2e75597806dae40f8db6d419a3b0156432d
Justin Studer <jstuder@tik43x.ee.ethz.ch>
2021-09-16 15:48:52 +0200

    updated run_script

--- run_script.sh
+++ run_script.sh
@@ -15,8 +15,8 @@
 set -o errexit
 
 source /itet-stor/jstuder/net_scratch/anaconda3/bin/activate ml
-#srun python /itet-stor/jstuder/net_scratch/nl-pl_moco/xMoCo_pl.py --shuffle --num_epochs=20 --learning_rate=1e-5 --debug_data_skip_interval 1 --effective_queue_size=4096 --effective_batch_size=32 --num_gpus=4  --accelerator="ddp"
-srun python /itet-stor/jstuder/net_scratch/nl-pl_moco/xMoCo_pl.py --effective_queue_size=4096 --effective_batch_size 32 --language="ruby" --debug_data_skip_interval=1 --always_use_full_val --shuffle --num_hard_negatives=2 --learning_rate=2e-5
+
+srun python /itet-stor/jstuder/net_scratch/nl-pl_moco/xMoCo_pl.py --effective_batch_size 32 --effective_queue_size=8192 --language="ruby" --debug_data_skip_interval=1 --shuffle --always_use_full_val --num_hard_negatives=8 --barlow_projector_dimension=0 --barlow_weight=7e-5 --barlow_lambda=0.005 --use_barlow
 
 echo finished at: `date`
 exit 0;
