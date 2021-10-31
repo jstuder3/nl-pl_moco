c02bfb9c5b90b496de208ea83e7d75539fdbd9f2
Justin Studer <jstuder@tikgpu06.ee.ethz.ch>
2021-10-31 12:16:37 +0100

    added (poor) console-only mode

--- interactive_tool.py
+++ interactive_tool.py
@@ -1,7 +1,6 @@
 # WIP: Basic shape works, next I'll have to add checkpoint loading and preprocessing (if uncached) and maybe a loading bar for preprocessing, if that is even possible
 # for evaluating, make a new xMoCoModelPTL with stripped-down functionality and use the test_dataloader to load the corresponding subsets. note that we only have to forward the code, not the docstrings
 
-from appJar import *
 import torch
 import torch.nn.functional as F
 from xMoCo_pl import xMoCoModelPTL
@@ -149,28 +148,117 @@ parser.add_argument("--debug_data_skip_interval", type=int, default=1)
 parser.add_argument("--always_use_full_val", default=True)
 parser.add_argument("--num_gpus", type=int, default=torch.cuda.device_count())
 parser.add_argument("--num_hard_negatives", type=int, default=0)
+parser.add_argument("--do_adv_test", default=False)
 args = parser.parse_args()
 
-app=gui("CodeSearch Tool", "1600x900")
-app.setPadding([5, 5])
-app.setSticky("ew")
-app.setFont(14)
+gui_mode=True
 
-app.addLabelEntry("Query: ", 0, 0, 1)
+if gui_mode:
 
-app.addButton("OK", getCodeSamples, 0, 1, 1)
-app.addLabelOptionBox("Language: ", ["Ruby", "JavaScript", "Java", "Go", "PHP", "Python"], 0, 2, 1)
+    from appJar import *
+    app=gui("CodeSearch Tool", "1600x900")
+    app.setPadding([5, 5])
+    app.setSticky("ew")
+    app.setFont(14)
 
-subset = {"Train": True, "Validation": True, "Test": True}
-app.addProperties("Subsets to search in: ", subset, 1, 2, 1)
+    app.addLabelEntry("Query: ", 0, 0, 1)
 
-app.addLabelScale("Number of outputs: ", 2, 2, 1)
-app.setScaleRange("Number of outputs: ", 1, 20)
-app.showScaleIntervals("Number of outputs: ", 19)
-app.showScaleValue("Number of outputs: ")
+    app.addButton("OK", getCodeSamples, 0, 1, 1)
+    app.addLabelOptionBox("Language: ", ["Ruby", "JavaScript", "Java", "Go", "PHP", "Python"], 0, 2, 1)
 
-app.addScrolledTextArea("outputs", 1, 0, 2, 2)
+    subset = {"Train": True, "Validation": True, "Test": True}
+    app.addProperties("Subsets to search in: ", subset, 1, 2, 1)
 
-app.go()
+    app.addLabelScale("Number of outputs: ", 2, 2, 1)
+    app.setScaleRange("Number of outputs: ", 1, 20)
+    app.showScaleIntervals("Number of outputs: ", 19)
+    app.showScaleValue("Number of outputs: ")
 
-app.go()
+    app.addScrolledTextArea("outputs", 1, 0, 2, 2)
+
+    app.go()
+
+    app.go()
+#meant for fast precomputation of the embeddings on the server
+else:
+    subsets = {"Train": True, "Validation": True, "Test": True}
+    language="php"
+    checkpoint = "Oct22_05_46_05-xMoCo-php"
+    query = "Print the current time"
+    num_samples = 1
+
+    if subsets["Train"]:
+        train_json=loadJson(args.base_data_folder+f"/{language}/train.jsonl")
+    if subsets["Validation"]:
+        val_json=loadJson(args.base_data_folder+f"/{language}/valid.jsonl")
+    if subsets["Test"]:
+        test_json=loadJson(args.base_data_folder+f"/{language}/test.jsonl")
+
+    print(query, language, num_samples, subsets)
+
+    # [CHECK IF EMBEDDINGS ARE CACHED]:
+    code_embeddings = {"train": torch.tensor([]).cuda(), "valid": torch.tensor([]).cuda(), "test": torch.tensor([]).cuda()}
+    for key in subsets:
+        if subsets[key]:
+            key = key.lower()
+            if key == "validation":
+                key = "valid"
+            try:
+                code_embeddings[key] = torch.load(f"cache/{language}_{key}.pt")
+            except:
+                print(f"No cached tensors found. Generating embeddings...")
+                if model==None or prev_language!=language:
+                    model=xMoCoModelPTL.load_from_checkpoint(f"checkpoints/{checkpoint}.ckpt")#{language}.ckpt") # note: change checkpoint to the desired language
+                    prev_language=language
+
+                dataloader=generateDataLoader(language, key, tokenizer, tokenizer, args)
+                code_embeddings[key]=generateEmbeddings(model, dataloader, key).type(torch.float16)
+                torch.save(code_embeddings[key], f"cache/{language}_{key}.pt")
+
+    assert code_embeddings["train"].shape[0]>0 or code_embeddings["valid"].shape[0]>0 or code_embeddings["test"].shape[0]>0, "Please select at least one subset to search in"
+
+    embeddings_matrix = torch.cat((code_embeddings["train"], code_embeddings["valid"], code_embeddings["test"])).type(torch.half)
+
+    if model == None or prev_language!=language:
+        model = xMoCoModelPTL.load_from_checkpoint(f"checkpoints/{language}.ckpt")
+        prev_language=language
+
+    # tokenize and encode the given docstring
+    query_tokenized = tokenizer(query, truncation=True, padding="max_length")
+
+    input_tokens=torch.tensor([query_tokenized["input_ids"]]).cuda()
+    input_mask=torch.tensor([query_tokenized["attention_mask"]]).cuda()
+
+    if model.docs_fast_encoder.device==torch.device("cpu"):
+        model.docs_fast_encoder.cuda()
+
+    if model.docs_fast_encoder.training:
+        model.docs_fast_encoder.eval()
+    with torch.no_grad():
+        query_embedding = model.docs_fast_encoder(input_ids=input_tokens, attention_mask=input_mask)["pooler_output"]
+        query_embedding = F.normalize(query_embedding, p=2, dim=1).type(torch.half)
+
+    similarity_matrix = torch.matmul(query_embedding, embeddings_matrix.T)
+    top_similarities, top_indices = torch.topk(similarity_matrix, num_samples)
+
+    after_str = "#############################################################################\n\n"
+    output_str = after_str
+
+    train_size = code_embeddings["train"].shape[0]
+    val_size = code_embeddings["valid"].shape[0]
+    test_size = code_embeddings["test"].shape[0]
+    for i, code_index in enumerate(top_indices[0].tolist()):
+        if code_index < code_embeddings["train"].shape[0]:
+            data=train_json[code_index]
+        elif code_index < train_size+val_size:
+            data=val_json[code_index-train_size]
+        elif code_index<train_size+val_size+test_size:
+            data=test_json[code_index-train_size-val_size]
+        else:
+            assert False, "Something went wrong..."
+        #confidence=torch.exp(top_similarities[0][i])/torch.sum(torch.exp(similarity_matrix))
+        header_str = f"{i+1}: (similarity: {top_similarities[0][i].item():.4f})\n{data['url']}\n\n"
+        output_str += header_str + data["docstring"]+ "\n\n" + data["code"] +"\n\n" + after_str
+
+    print(output_str)
+
